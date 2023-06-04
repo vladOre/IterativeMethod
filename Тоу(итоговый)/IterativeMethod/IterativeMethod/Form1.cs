752199d2199d2c36f78974924a8de0ccc71737b8
vladOre <102778837+vladOre@users.noreply.github.com>
2023-06-04 19:42:36 +0500

    Add files via upload

---
+++ Тоу(итоговый)/IterativeMethod/IterativeMethod/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IterativeMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {        
+                //Задаем начальные значения
+                chart1.Series[0].Points.Clear();
+                chart1.Series[1].Points.Clear();
+                chart1.Series[2].Points.Clear();
+                int Count_Points = 1000;
+                double Null_value_X = 5;
+                double Epsilon_Y = 0.00000001;
+                double Epsilon_X = 0.000000001;
+                double Left_border = 0;
+                double Right_border = 1;
+                double Null_lambda = 1;
+                double Delta_border_T = Math.Abs(Left_border - Right_border) / Count_Points;
+                double J0 = 0, J1 = 0;
+                bool division = false;
+                double[,] u_delta_T = new double[2, Count_Points];
+                double[,] x_delta_T = new double[2, Count_Points];
+                double[] J_delta_T = new double[2];
+                double[] lambda;
+                int First_round, round = -1;
+                for (First_round=0; ; First_round++)
+                {
+                    round++;
+                    double[] u0 = new double[Count_Points];// нулевой вектор
+                    //for (int i = 0; i < Count_Points; i++)
+                    //    u0[i] = 1;/* единичный вектор*/
+                    double[] x0 = new double[Count_Points];            
+                    double[] Sort_X = new double[Count_Points];
+                    double[] Sort_X1 = new double[Count_Points/2];
+                    lambda = new double[Count_Points];
+                    lambda[0] = 1;
+                    x0[0] = Null_value_X;
+                    for (int i = 0; i < Count_Points - 1; i++)                 
+                        x0[i+1] = x0[i] + Delta_border_T * u0[i];                    //пересчет исксов
+                    J0 = Quality_Functional(x0, u0, Count_Points, Delta_border_T);
+                    //лямба q-тая равна 0
+                    for(int Second_round=0; Second_round<1;)
+                    {
+                        lambda[Count_Points - 1] = 0 - Null_lambda * x0[Count_Points - 1] * Delta_border_T;
+                        for (int i = Count_Points - 2; i > 0; i--)                         
+                            lambda[i] = lambda[i+1] - Null_lambda * x0[i] * Delta_border_T;                        
+                        double[] u1 = new double[Count_Points];//!
+                        double[] x1 = new double[Count_Points];//!
+                        x1[0] = Null_value_X;
+                        for (int i = 0; i < Count_Points - 1; i++)
+                            u1[i] = lambda[i + 1] / Null_lambda;
+                        u1[Count_Points - 1] = 0;
+                        for (int i = 0; i < Count_Points - 1; i++)
+                            x1[i+1] = x1[i] + Delta_border_T * u1[i];
+                        J1= Quality_Functional(x1, u1, Count_Points, Delta_border_T);
+                        for (int i = 0; i < Count_Points; i++)
+                            Sort_X[i] = Math.Abs(x1[i] - x0[i]);
+                        Array.Sort(Sort_X);
+                        if(J1<J0)
+                        {
+                            if (Math.Abs(J1 - J0) < Epsilon_Y && Sort_X[Count_Points-1] < Epsilon_X)                           
+                                Second_round = 2;
+                            else
+                            {
+                                J0 = J1;
+                                for (int i = 0; i < Count_Points; i++)
+                                {
+                                    x0[i] = x1[i];
+                                    u0[i] = u1[i];
+                                }
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            J1 = J0;
+                            for (int i = 0; i < Count_Points; i++)
+                            {
+                                x1[i] = x0[i];
+                                u1[i] = u0[i];
+                            }
+                            Second_round = 2;
+                        }
+                        J_delta_T[First_round % 2] = J1;
+                        for (int j = 0; j < Count_Points; j++)
+                        {
+                            x_delta_T[First_round % 2, j] = x1[j];
+                            u_delta_T[First_round % 2, j] = u1[j];
+                        }
+                    }
+                    if(division == false)        // делили мы шаг       
+                        division = true;                    
+                    else
+                    {
+                        for (int i = 0; i < Count_Points / 2; i++)
+                            Sort_X1[i] = Math.Abs(x_delta_T[0, i] - x_delta_T[1, 2 * i]);
+                        Array.Sort(Sort_X1);
+                        if (Math.Abs(J_delta_T[0] - J_delta_T[1]) < Epsilon_Y && Sort_X1[Count_Points / 2 - 1] < Epsilon_X)
+                            break;                        
+                        else
+                        {
+                            J_delta_T[Math.Abs(First_round % 2 - 1)] = J_delta_T[First_round % 2];
+                            for (int j = 0; j < Count_Points; j++)
+                            {
+                                x_delta_T[Math.Abs(First_round % 2 - 1), j] = x_delta_T[First_round % 2, j];
+                                u_delta_T[Math.Abs(First_round % 2 - 1), j] = u_delta_T[First_round % 2, j];
+                            }
+                            First_round--;
+                        }
+                    }
+                    if (Count_Points > 1000000)// проверка на размерность массива чтобы не был больше ляма
+                         break;
+                    // перекладывание значений
+                    Count_Points = 2 * Count_Points;
+                    Delta_border_T = Math.Abs(Left_border - Right_border) / Count_Points;
+                    double[,] service_u_delta_T = new double[2, Count_Points];
+                    double[,] service_x_delta_T = new double[2, Count_Points];
+                    for (int j = 0; j < Count_Points/2; j++)
+                    {
+                        service_u_delta_T[0, j] = u_delta_T[0, j];
+                        service_u_delta_T[1, j] = u_delta_T[1, j];
+                        service_x_delta_T[0, j] = x_delta_T[0, j];
+                        service_x_delta_T[1, j] = x_delta_T[1, j];
+                    }
+                    u_delta_T = new double[2, Count_Points];
+                    x_delta_T = new double[2, Count_Points];
+                    for (int j = 0; j < Count_Points / 2; j++)
+                    {
+                        u_delta_T[0, j] = service_u_delta_T[0, j];
+                        u_delta_T[1, j] = service_u_delta_T[1, j];
+                        x_delta_T[0, j] = service_x_delta_T[0, j];
+                        x_delta_T[1, j] = service_x_delta_T[1, j];
+                    }
+                }
+                //вывод отрисовка
+                textBox1.Text = $"{round}";
+                textBox2.Text = $"{J_delta_T[First_round % 2]}";
+                int s = 0;
+                for (double k = Left_border; k < Right_border; k += 0.001)
+                {
+                    chart1.Series[0].Points.AddXY(k, x_delta_T[First_round % 2, s]);// численный x
+                    chart1.Series[1].Points.AddXY(k, u_delta_T[First_round % 2, s]);
+                    chart1.Series[2].Points.AddXY(k, ((Math.Pow(x_delta_T[First_round % 2, s], 2) + Math.Pow(u_delta_T[First_round % 2, s], 2)) / -2) +  u_delta_T[First_round % 2, s] * lambda[s]);// внимательно функция понтрягина
+                    chart1.Series[3].Points.AddXY(k, 0.6*Math.Exp(k)+ 4.42*Math.Exp(-k)); //формула для икса
+                    s += Count_Points / 1000;/*Convert.ToInt32(k * 1000);*//* Convert.ToInt32(k*1000/Delta_border_T);*/
+                }                
+                this.Refresh();                
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Ошибка!");
+            }
+        }
+        private double Quality_Functional(double[] x0, double[] u0, int Count_Points, double Delta_border_T)
+        {
+            double J = 0;
+            for (int i = 0; i < Count_Points; i++) 
+            {
+                J += (Math.Pow(u0[i], 2) + Math.Pow(x0[i], 2)) * Delta_border_T;
+            }
+            J /= 2;
+            return J;
+        }
+    }
+}
---
+++ Тоу(итоговый)1/IterativeMethod/IterativeMethod/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace IterativeMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(118, 12);
+            this.chart1.Name = "chart1";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.Legend = "Legend1";
+            series7.Name = "x(t)";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series8.Legend = "Legend1";
+            series8.Name = "u(t)";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series9.Legend = "Legend1";
+            series9.Name = "H(t,x,u)";
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Series.Add(series9);
+            this.chart1.Size = new System.Drawing.Size(670, 426);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "IterativeMethod";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
